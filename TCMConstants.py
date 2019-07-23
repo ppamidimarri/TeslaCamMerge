b58fcc8daa6d53da1a2083625253c0e4985d9f1c
ppamidimarri <41935734+ppamidimarri@users.noreply.github.com>
2019-07-23 08:14:10 -0700

    Added comments

--- TCMConstants.py
+++ TCMConstants.py
@@ -1,16 +1,27 @@
 import logging
 
+# Location where the TeslaCamMerge directory is present. Must NOT include trailing /
+PROJECT_PATH = '/home/pavan'
+
+# Locations of the stored footage on the SSD. MUST include trailing /
 FULL_PATH = '/media/pavan/Samsung_T5/Footage/Full/'
 RAW_PATH = '/media/pavan/Samsung_T5/Footage/Raw/'
 FAST_PATH = '/media/pavan/Samsung_T5/Footage/Fast/'
 UPLOAD_LOCAL_PATH = '/media/pavan/Samsung_T5/Footage/Upload/'
+
+# Location of CIFS share. MUST include trailing /
 SHARE_PATH = '/samba/fjnuser/'
+
+# Location of rclone entry for Google Drive
 UPLOAD_REMOTE_PATH = 'gdrive:/TeslaCam'
-LOG_PATH = '/home/pavan/log/'
+
+# Settings for application logs
+LOG_PATH = '/home/pavan/log/' # Must include trailing /
 LOG_EXTENSION = '.log'
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOG_LEVEL = logging.DEBUG
+
+# Paths of installed software, including name of the application
 FFMPEG_PATH = '/usr/bin/ffmpeg'
 RCLONE_PATH = '/usr/local/bin/rclone'
-PROJECT_PATH = '/home/pavan'
 FILEBROWSER_PATH = '/usr/local/bin/filebrowser'

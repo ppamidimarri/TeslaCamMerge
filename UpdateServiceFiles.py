9d139e2432dd874957d292464d387936fdf5fd72
ppamidimarri <41935734+ppamidimarri@users.noreply.github.com>
2019-07-23 08:47:13 -0700

    Multiple replacements

--- UpdateServiceFiles.py
+++ UpdateServiceFiles.py
@@ -1,18 +1,31 @@
 import os
+import re
 import TCMConstants
 
+replacements = {
+	"FILEBROWSER_PATH" : TCMConstants.FILEBROWSER_PATH,
+	"SSD_MOUNT_POINT" : TCMConstants.SSD_MOUNT_POINT,
+	"PROJECT_PATH" : TCMConstants.PROJECT_PATH,
+	"PROJECT_USER" : TCMConstants.PROJECT_USER
+}
+
 def main():
-  process_service_file('loadSSD.service')
-  process_service_file('mergeTeslaCam.service')
-  process_service_file('uploadDrive.service')
-  process_service_file('startFileBrowser.service')
+	process_service_file('loadSSD.service')
+	process_service_file('mergeTeslaCam.service')
+	process_service_file('uploadDrive.service')
+	process_service_file('startFileBrowser.service')
 
 def process_service_file(name):
-  with open(name, "rt") as fin:
-    with open(name + ".tmp", "wt") as fout:
-       for line in fin:
-           fout.write(line.replace('PROJECT_PATH', PROJECT_PATH).replace('FILEBROWSER_PATH', FILEBROWSER_PATH))
-  os.rename(name + ".tmp", name)
-  
+	with open(name, "rt") as fin:
+		with open(name + ".tmp", "wt") as fout:
+			for line in fin:
+				fout.write(do_replacements(line))
+	os.rename(name + ".tmp", name)
+
+def do_replacements(line):
+	substrs = sorted(replacements, key=len, reverse=True)
+	regexp = re.compile('|'.join(map(re.escape, substrs)))
+	return regexp.sub(lambda match: replacements[match.group(0)], line)
+
 if __name__ == '__main__':
 	main()
